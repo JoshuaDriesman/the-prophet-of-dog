9ce785d639d21fbbd7cda86d306fac171ff9ea54
Joshua Driesman <jdriesman@outlook.com>
2023-05-06 18:00:46 -0400

    fixed response

--- backend/packages/mailing-list/subscribe/subscribe.go
+++ backend/packages/mailing-list/subscribe/subscribe.go
@@ -7,14 +7,17 @@ type Event struct {
 }
 
 type Response struct {
-	Body string `json:"body"`
+	Body       string `json:"body"`
+	StatusCode int 	  `json:"statusCode"`
 }
 
 func Main(ctx context.Context, event Event) Response {
 	msg := make(map[string]interface{})
 	if event.Email == "" {
-		msg["statusCode"] = 400
-		msg["body"] = "Missing email"
+		return Response{
+			StatusCode: 400,
+			Body: "Missing email"
+		}
 	}
 
 	return Response{
