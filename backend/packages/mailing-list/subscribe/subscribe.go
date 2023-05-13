0ebf473e5cf02774aa50f45eba2abdb18ec75cc6
Joshua Driesman <jdriesman@outlook.com>
2023-05-13 08:40:16 -0400

    added db logic

--- backend/packages/mailing-list/subscribe/subscribe.go
+++ backend/packages/mailing-list/subscribe/subscribe.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"context"
+	"database/sql"
+	"log"
+	"os"
 	"regexp"
+
+	"github.com/google/uuid"
 )
 
 type Event struct {
@@ -29,6 +34,43 @@ func Main(ctx context.Context, event Event) Response {
 		return Response{
 			StatusCode: 400,
 			Body:       "Email is invalid",
+		}
+	}
+
+	db, connErr := sql.Open("postgres", os.Getenv("DB_CONNECTION_INFO"))
+	if connErr != nil {
+		log.Fatalf("Can't connect to DB: %s", connErr)
+		return Response{
+			StatusCode: 500,
+			Body:       "DB connection error",
+		}
+	}
+	defer db.Close()
+
+	// Check if email already exists
+	rows, selectErr := db.Query("SELECT * FROM subscribers WHERE email = %s;", event.Email)
+	if selectErr != nil {
+		log.Fatalf("Could not check subscriber %s: %s", event.Email, selectErr)
+		return Response{
+			StatusCode: 500,
+			Body:       "DB query error",
+		}
+	}
+	defer rows.Close()
+	if rows.Next() {
+		return Response{
+			StatusCode: 400,
+			Body:       "Email already exists.",
+		}
+	}
+
+	newEmailID := uuid.NewString()
+	_, insertErr := db.Exec("INSERT INTO subscribers (id, email, name, subscribed) VALUES (%s, %s, %s, %t);", newEmailID, event.Email, event.Name, true)
+	if insertErr != nil {
+		log.Fatalf("Could not insert subscriber %s: %s", event.Email, connErr)
+		return Response{
+			StatusCode: 500,
+			Body:       "DB query error",
 		}
 	}
 
