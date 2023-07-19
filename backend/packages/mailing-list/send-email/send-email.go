ac06db1067830893ea78f2f4d5ba06d79892b297
Joshua Driesman <jdriesman@outlook.com>
2023-07-19 09:02:44 -0400

    logging tweak

--- backend/packages/mailing-list/send-email/send-email.go
+++ backend/packages/mailing-list/send-email/send-email.go
@@ -127,7 +127,7 @@ func Main(ctx context.Context, event Event) Response {
 	}
 	if sendgridBatchIdResponse.StatusCode != 200 {
 		systemErrorResp.Body = "sendgrid batch id could not be retrieved"
-		// log.Fatal("Could not retrieve sendgrid batch ID")
+		log.Print("Could not retrieve sendgrid batch ID")
 		return systemErrorResp
 	}
 
